76c6f1f3153776e867130e48c4b099e214d1980d
John Dyer <lbrapid@gmail.com>
2014-01-23 09:52:08 -0500

    WIP ProductImporter

--- lib/open_erp/product_importer.rb
+++ lib/open_erp/product_importer.rb
@@ -2,6 +2,19 @@ module OpenErp
   class ProductImporter
     class << self
       def run!
+        products = ProductProduct.find(:all, :domain => ['waiting_spree_import', '=', true])
+        result = []
+
+        products.each do |product|
+          result << ProductImporter.product_to_product_import_message(product)
+          product.waiting_spree_import = false
+          product.save
+        end
+
+      end
+
+      def product_to_product_import_message(product)
+
       end
     end
   end
--- spec/lib/open_erp/product_importer_spec.rb
+++ spec/lib/open_erp/product_importer_spec.rb
@@ -1,15 +1,26 @@
 require 'spec_helper'
 
+ProductProduct = OpenStruct.new
+
 describe OpenErp::ProductImporter do
-  before(:all) do
-    VCR.use_cassette('ooor') do
-      Ooor.new url: ENV['OERP_URL'],
-               database: ENV['OERP_DB'],
-               username: ENV['OERP_USER'],
-               password: ENV['OERP_PASS']
-    end
+  let(:product) do
+    OpenStruct.new
   end
 
   describe '.run!' do
+    context 'when there are products to import' do
+      it 'returns an array of product:import messages' do
+        ProductProduct.should_receive(:find).exactly(:once).and_return([product])
+      end
+    end
+
+    context 'when there are no products to import' do
+      it 'returns an empty array' do
+        ProductProduct.should_receive(:find).exactly(:once).and_return([])
+        result = described_class.run!
+        result.should be_a Array
+        result.should be_empty
+      end
+    end
   end
 end
