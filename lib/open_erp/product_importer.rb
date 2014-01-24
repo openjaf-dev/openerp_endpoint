56f0588cd5fc6c1c8526f7eeec77a73a5e5f6497
John Dyer <lbrapid@gmail.com>
2014-01-24 10:28:25 -0500

    Finish ProductImporter

--- lib/open_erp/product_importer.rb
+++ lib/open_erp/product_importer.rb
@@ -5,16 +5,58 @@ module OpenErp
         products = ProductProduct.find(:all, :domain => ['waiting_spree_import', '=', true])
         result = []
 
-        products.each do |product|
+        non_variant_products = products.find_all { |p| p.variants.nil? }
+        variant_products = products.find_all { |p| !p.variants.nil? }
+
+        non_variant_products.each do |product|
           result << ProductImporter.product_to_product_import_message(product)
           product.waiting_spree_import = false
           product.save
         end
 
+        variant_products.each do |product|
+          result << ProductImporter.variant_product_to_product_import_message(product)
+          product.waiting_spree_import = false
+          product.save
+        end
+
+        result
       end
 
       def product_to_product_import_message(product)
+        {
+          product: {
+            sku: product.default_code,
+            name: product.name,
+            price: product.list_price,
+            description: product.description
+          }
+        }
+      end
 
+      def variant_product_to_product_import_message(product)
+        {
+          product: {
+            sku: product.permalink,
+            name: product.name,
+            price: product.list_price,
+            description: product.description,
+            variants: [
+              {
+                name: product.variants,
+                sku: product.default_code,
+                price: product.list_price,
+                options: [{
+                  ProductImporter.hyphenate_variant(product.variants) => product.variants
+                }]
+              }
+            ]
+          }
+        }
+      end
+
+      def hyphenate_variant(name)
+        name.downcase.gsub(' ', '-')
       end
     end
   end
--- spec/lib/open_erp/product_importer_spec.rb
+++ spec/lib/open_erp/product_importer_spec.rb
@@ -4,13 +4,51 @@ ProductProduct = OpenStruct.new
 
 describe OpenErp::ProductImporter do
   let(:product) do
-    OpenStruct.new
+    OpenStruct.new name: "A product",
+      description: "I have no variants",
+      price: 20.99,
+      default_code: "FOOBAR"
+  end
+  let(:variant_product) do
+    OpenStruct.new name: "A product",
+      description: "I have a variant",
+      permalink: "i-am-a-variant",
+      price: 20.99,
+      default_code: "FOOBAR1",
+      variants: "A Variant"
   end
 
   describe '.run!' do
     context 'when there are products to import' do
       it 'returns an array of product:import messages' do
-        ProductProduct.should_receive(:find).exactly(:once).and_return([product])
+        ProductProduct.should_receive(:find).exactly(:once).and_return([product, variant_product])
+        result = described_class.run!
+        result.should be_a Array
+        result.length.should == 2
+        result.first[:product][:name].should == product.name
+        result.last[:product][:variants].first[:name].should == variant_product.variants
+      end
+
+      context 'when a variant is found' do
+        before(:each) { ProductProduct.should_receive(:find).exactly(:once).and_return([variant_product]) }
+
+        it 'returns a product:import message with an embedded variant' do
+          result = described_class.run!
+          result.should be_a Array
+          result.length.should == 1
+          result.first[:product][:variants].first[:name].should == variant_product.variants
+        end
+
+        it 'uses the permalink as the master product sku' do
+          result = described_class.run!
+          result.first[:product][:name].should == variant_product.name
+        end
+
+        it "returns proper options for the variant" do
+          result = described_class.run!
+          result.first[:product][:variants].first[:options].first.first[0].should == "a-variant"
+          result.first[:product][:variants].first[:options].first.first[1].should == variant_product.variants
+        end
       end
     end
 
